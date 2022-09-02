14cf43e3f0e39cacfa7c82152d02fb6572fcc71a
Alexander Brandes <brandes.alexander@web.de>
2022-09-02 21:49:11 +0200

    Document the use of a custom Jenkins home directory (#21)

--- checkout-and-start.sh
+++ checkout-and-start.sh
@@ -26,5 +26,11 @@ fi
 
 mvn --no-transfer-progress -V -pl war --also-make clean package -DskipTests -Dmaven.test.skip=true -P quick-build
 
+if [[ -n ${JENKINS_HOME-} ]]; then
+    echo "Jenkins home directory changed to $JENKINS_HOME"
+else
+    echo "Using default home directory"
+fi
+
 echo "Starting Jenkins"
 java -jar war/target/jenkins.war
